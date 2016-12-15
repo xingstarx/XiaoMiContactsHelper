1387c4d681308a31bf2bb5ccaac40c8f270d7d32
xingxing <xxx823952375@gmail.com>
2016-12-16 00:34:01 +0800

    contact sort by SORT_KEY_PRIMARY

--- app/src/main/java/com/star/contacts/MainActivity.java
+++ app/src/main/java/com/star/contacts/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Contract> getContracts(ContentResolver cr) {
         List<Contract> contracts = new ArrayList<>();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.Contacts.SORT_KEY_PRIMARY);
         if (cur.getCount() > 0) {
             while (cur.moveToNext()) {
                 String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
